53a9490d3ae07a56817a37728e0b1185e232fc21
Dylan Powers <dylan.powers@email.wsu.edu>
2013-10-09 18:22:28 -0700

    Basic C++ code added

---
+++ OpenGLSandbox/src/main/jni/renderer.cpp
@@ -0,0 +1,5 @@
+#include "renderer.h"
+
+Renderer::Renderer() {
+  int b = 1 + 3;
+}
--- OpenGLSandbox/src/main/jni/renderer.h
+++ OpenGLSandbox/src/main/jni/renderer.h
@@ -1,7 +1,11 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+class Renderer {
 
+public:
+  Renderer();
+}
 
 
 #endif
