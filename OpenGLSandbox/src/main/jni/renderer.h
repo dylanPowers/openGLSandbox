656a3e7c7fbdde388d2d1777ea245c4c4d839e7e
Dylan Powers <dylan.kyle.powers@gmail.com>
2013-10-10 09:25:12 -0700

    Attempt at native code

--- OpenGLSandbox/src/main/jni/renderer.h
+++ OpenGLSandbox/src/main/jni/renderer.h
@@ -5,34 +5,41 @@
 #include <EGL/egl.h>
 
 class Renderer {
-public:
-  static void onDrawFrame() {
-    glClear(GL_COLOR_BUFFER_BIT);
-  }
 
-  static void onSurfaceCreated() {
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-  }
 
-  static void onSurfaceChanged(int width, int height) {
-    glViewport(0, 0, width, height);
-
-    float left = -1.0;
-    float right = 1.0;
-    float bottom = -1.0;
-    float top = 1.0;
-    const float near = 1.0;
-    const float far = 10.0;
-
-    float ratio = (float) width / height;
-    if (ratio > 1.0) {
-      left = -ratio;
-      right = ratio;
-    } else {
-      bottom = - (1 / ratio);
-      top = 1 / ratio;
+  public:
+    static void onDrawFrame() {
+      glClear(GL_COLOR_BUFFER_BIT);
     }
-  }
+
+    static void onSurfaceCreated() {
+      glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    static void onSurfaceChanged(int width, int height) {
+      glViewport(0, 0, width, height);
+
+      float left = -1.0;
+      float right = 1.0;
+      float bottom = -1.0;
+      float top = 1.0;
+      const float near = 1.0;
+      const float far = 10.0;
+
+      float ratio = (float) width / height;
+      if (ratio > 1.0) {
+        left = -ratio;
+        right = ratio;
+      } else {
+        bottom = - (1 / ratio);
+        top = 1 / ratio;
+      }
+
+      mat4::frustum(m_projection_matrix, 0, left, right, bottom, top, near, far);
+    }
+
+  private:
+    static mat4 m_projection_matrix = mat4();
 };
 
 
