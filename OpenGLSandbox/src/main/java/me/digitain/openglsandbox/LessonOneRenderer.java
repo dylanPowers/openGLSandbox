5fdfbeced854f97b7a5f874b4d9ca1b3dc67f1a4
Dylan Powers <dylan.kyle.powers@gmail.com>
2013-10-16 00:09:22 -0700

    Added a switch for running C++ versus Java code

--- OpenGLSandbox/src/main/java/me/digitain/openglsandbox/LessonOneRenderer.java
+++ OpenGLSandbox/src/main/java/me/digitain/openglsandbox/LessonOneRenderer.java
@@ -22,10 +22,12 @@ import javax.microedition.khronos.opengles.GL10;
 public class LessonOneRenderer implements GLSurfaceView.Renderer {
   private static final String TAG = "OpenGLSandbox Renderer";
 
+  private static final boolean RUN_NATIVE = false;
+
   // Model data in float buffers
-//  private final FloatBuffer mTriangle1Vertices;
-//  private final FloatBuffer mTriangle2Vertices;
-//  private final FloatBuffer mTriangle3Vertices;
+  private final FloatBuffer mTriangle1Vertices;
+  private final FloatBuffer mTriangle2Vertices;
+  private final FloatBuffer mTriangle3Vertices;
 
   private final int mBytesPerFloat = 4; // DUH!!!?????
 
@@ -38,57 +40,57 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
     mAssetsManager = assets;
 
 
-//
-//    // A red, green, and blue triangle
-//    final float[] triangle1VerticesData = {
-//        // X, Y, Z,
-//        // R, G, B, A
-//        -0.5f, -0.25f, 0.0f,
-//        1.0f, 0.0f, 0.0f, 1.0f,
-//
-//        0.5f, -0.25f, 0.0f,
-//        0.0f, 0.0f, 1.0f, 1.0f,
-//
-//        0.0f, 0.559016994f, 0.0f,
-//        0.0f, 1.0f, 0.0f, 1.0f
-//    };
-//
-//    // This triangle is yellow, cyan, and magenta.
-//    final float[] triangle2VerticesData = {
-//        // X, Y, Z,
-//        // R, G, B, A
-//        -0.5f, -0.25f, 0.0f,
-//        1.0f, 1.0f, 0.0f, 1.0f,
-//
-//        0.5f, -0.25f, 0.0f,
-//        0.0f, 1.0f, 1.0f, 1.0f,
-//
-//        0.0f, 0.559016994f, 0.0f,
-//        1.0f, 0.0f, 1.0f, 1.0f
-//    };
-//
-//    // This triangle is white, gray, and black.
-//    final float[] triangle3VerticesData = {
-//        // X, Y, Z,
-//        // R, G, B, A
-//        -0.5f, -0.25f, 0.0f,
-//        1.0f, 1.0f, 1.0f, 1.0f,
-//
-//        0.5f, -0.25f, 0.0f,
-//        0.5f, 0.5f, 0.5f, 1.0f,
-//
-//        0.0f, 0.559016994f, 0.0f,
-//        0.0f, 0.0f, 0.0f, 1.0f
-//    };
-//
-//    // Init our buffers
-//    mTriangle1Vertices = allocateVertexBuffer(triangle1VerticesData);
-//    mTriangle2Vertices = allocateVertexBuffer(triangle2VerticesData);
-//    mTriangle3Vertices = allocateVertexBuffer(triangle3VerticesData);
-//
-//    mTriangle1Vertices.put(t riangle1VerticesData).position(0);
-//    mTriangle2Vertices.put(triangle2VerticesData).position(0);
-//    mTriangle3Vertices.put(triangle3VerticesData).position(0);
+
+    // A red, green, and blue triangle
+    final float[] triangle1VerticesData = {
+        // X, Y, Z,
+        // R, G, B, A
+        -0.5f, -0.25f, 0.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+
+        0.5f, -0.25f, 0.0f,
+        0.0f, 0.0f, 1.0f, 1.0f,
+
+        0.0f, 0.559016994f, 0.0f,
+        0.0f, 1.0f, 0.0f, 1.0f
+    };
+
+    // This triangle is yellow, cyan, and magenta.
+    final float[] triangle2VerticesData = {
+        // X, Y, Z,
+        // R, G, B, A
+        -0.5f, -0.25f, 0.0f,
+        1.0f, 1.0f, 0.0f, 1.0f,
+
+        0.5f, -0.25f, 0.0f,
+        0.0f, 1.0f, 1.0f, 1.0f,
+
+        0.0f, 0.559016994f, 0.0f,
+        1.0f, 0.0f, 1.0f, 1.0f
+    };
+
+    // This triangle is white, gray, and black.
+    final float[] triangle3VerticesData = {
+        // X, Y, Z,
+        // R, G, B, A
+        -0.5f, -0.25f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f,
+
+        0.5f, -0.25f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f,
+
+        0.0f, 0.559016994f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    // Init our buffers
+    mTriangle1Vertices = allocateVertexBuffer(triangle1VerticesData);
+    mTriangle2Vertices = allocateVertexBuffer(triangle2VerticesData);
+    mTriangle3Vertices = allocateVertexBuffer(triangle3VerticesData);
+
+    mTriangle1Vertices.put(triangle1VerticesData).position(0);
+    mTriangle2Vertices.put(triangle2VerticesData).position(0);
+    mTriangle3Vertices.put(triangle3VerticesData).position(0);
   }
 
   private FloatBuffer allocateVertexBuffer(float[] verticesData) {
@@ -167,61 +169,64 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
   private int mColorHandle;
   @Override
   public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-//    RendererJniWrapper.onSurfaceCreated();
-//    GLES20.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
-//
-//    final float eyeX = 0.0f;
-//    final float eyeY = 0.0f;
-//    final float eyeZ = 1.5f;
-//
-//    final float lookX = 0.0f;
-//    final float lookY = 0.0f;
-//    final float lookZ = -5.0f;
-//
-//    final float upX = 0.0f;
-//    final float upY = 1.0f;
-//    final float upZ = 0.0f;
-//
-//    Matrix.setLookAtM(mViewMatrix, 0,
-//                      eyeX, eyeY, eyeZ,
-//                      lookX, lookY, lookZ,
-//                      upX, upY, upZ);
-//
     final String vertexShader = readAsset("VertexShader.glsl");
     final String fragmentShader = readAsset("FragmentShader.glsl");
-    RendererJniWrapper.onSurfaceCreated(fragmentShader, vertexShader);
-//
-//    int vertexShaderHandle = createShader(vertexShader, GLES20.GL_VERTEX_SHADER);
-//    int fragmentShaderHandle = createShader(fragmentShader, GLES20.GL_FRAGMENT_SHADER);
-//
-//    int programHandle = GLES20.glCreateProgram();
-//
-//    if (programHandle != 0) {
-//      GLES20.glAttachShader(programHandle, vertexShaderHandle);
-//      GLES20.glAttachShader(programHandle, fragmentShaderHandle);
-//
-//      GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
-//      GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
-//
-//      GLES20.glLinkProgram(programHandle);
-//
-//      final int[] linkStatus = new int[1];
-//      GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
-//      if (linkStatus[0] == 0)
-//      {
-//        GLES20.glDeleteProgram(programHandle);
-//        programHandle = 0;
-//      }
-//    }
-//    if (programHandle == 0) {
-//      throw new RuntimeException("Error creating the GLES 2.0 program.");
-//    }
-//
-//    mMVPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
-//    mPositionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
-//    mColorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
-//
-//    GLES20.glUseProgram(programHandle);
+    if (RUN_NATIVE) {
+      RendererJniWrapper.onSurfaceCreated(fragmentShader, vertexShader);
+    } else {
+      GLES20.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+
+      final float eyeX = 0.0f;
+      final float eyeY = 0.0f;
+      final float eyeZ = 1.5f;
+
+      final float lookX = 0.0f;
+      final float lookY = 0.0f;
+      final float lookZ = -5.0f;
+
+      final float upX = 0.0f;
+      final float upY = 1.0f;
+      final float upZ = 0.0f;
+
+      Matrix.setLookAtM(mViewMatrix, 0,
+                        eyeX, eyeY, eyeZ,
+                        lookX, lookY, lookZ,
+                        upX, upY, upZ);
+
+
+
+      int vertexShaderHandle = createShader(vertexShader, GLES20.GL_VERTEX_SHADER);
+      int fragmentShaderHandle = createShader(fragmentShader, GLES20.GL_FRAGMENT_SHADER);
+
+      int programHandle = GLES20.glCreateProgram();
+
+      if (programHandle != 0) {
+        GLES20.glAttachShader(programHandle, vertexShaderHandle);
+        GLES20.glAttachShader(programHandle, fragmentShaderHandle);
+
+        GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
+        GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
+
+        GLES20.glLinkProgram(programHandle);
+
+        final int[] linkStatus = new int[1];
+        GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+        if (linkStatus[0] == 0)
+        {
+          GLES20.glDeleteProgram(programHandle);
+          programHandle = 0;
+        }
+      }
+      if (programHandle == 0) {
+        throw new RuntimeException("Error creating the GLES 2.0 program.");
+      }
+
+      mMVPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
+      mPositionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
+      mColorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
+
+      GLES20.glUseProgram(programHandle);
+    }
   }
 
 
@@ -229,26 +234,29 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
 
   @Override
   public void onSurfaceChanged(GL10 gl10, int width, int height) {
-    RendererJniWrapper.onSurfaceChanged(width, height);
-//    GLES20.glViewport(0, 0, width, height);
-//
-//    float left = -1.0f;
-//    float right = 1.0f;
-//    float bottom = -1.0f;
-//    float top = 1.0f;
-//    final float near = 1.0f;
-//    final float far = 10.0f;
-//
-//    final float ratio = (float) width / height;
-//    if (ratio > 1.0) { // Horizontal
-//      left = -ratio;
-//      right = ratio;
-//    } else { // Vertical
-//      bottom = - (1 / ratio);
-//      top = 1 / ratio;
-//    }
-//
-//    Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
+    if (RUN_NATIVE) {
+      RendererJniWrapper.onSurfaceChanged(width, height);
+    } else {
+      GLES20.glViewport(0, 0, width, height);
+
+      float left = -1.0f;
+      float right = 1.0f;
+      float bottom = -1.0f;
+      float top = 1.0f;
+      final float near = 1.0f;
+      final float far = 10.0f;
+
+      final float ratio = (float) width / height;
+      if (ratio > 1.0) { // Horizontal
+        left = -ratio;
+        right = ratio;
+      } else { // Vertical
+        bottom = - (1 / ratio);
+        top = 1 / ratio;
+      }
+
+      Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
+    }
   }
 
 
@@ -256,27 +264,30 @@ public class LessonOneRenderer implements GLSurfaceView.Renderer {
 
   @Override
   public void onDrawFrame(GL10 gl10) {
-    RendererJniWrapper.onDrawFrame();
-//    GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
-//
-//    long time = SystemClock.uptimeMillis() % 5000L;
-//    float angleInDregrees = (360.0f / 5000.0f) * ((int) time);
-//
-//    Matrix.setIdentityM(mModelMatrix, 0);
-//    Matrix.rotateM(mModelMatrix, 0, angleInDregrees, 0.0f, 0.0f, 1.0f);
-//    drawTriangle(mTriangle1Vertices);
-//
-//    Matrix.setIdentityM(mModelMatrix, 0);
-//    Matrix.translateM(mModelMatrix, 0, 0.0f, -1.0f, 0.0f);
-//    Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
-//    Matrix.rotateM(mModelMatrix, 0, angleInDregrees, 0.0f, 0.0f, 1.0f);
-//    drawTriangle(mTriangle2Vertices);
-//
-//    Matrix.setIdentityM(mModelMatrix, 0);
-//    Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
-//    Matrix.rotateM(mModelMatrix, 0, 90.0f, 0.0f, 1.0f, 0.0f);
-//    Matrix.rotateM(mModelMatrix, 0, angleInDregrees, 0.0f, 0.0f, 1.0f);
-//    drawTriangle(mTriangle3Vertices);
+    if (RUN_NATIVE) {
+      RendererJniWrapper.onDrawFrame();
+    } else {
+      GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+
+      long time = SystemClock.uptimeMillis() % 5000L;
+      float angleInDregrees = (360.0f / 5000.0f) * ((int) time);
+
+      Matrix.setIdentityM(mModelMatrix, 0);
+      Matrix.rotateM(mModelMatrix, 0, angleInDregrees, 0.0f, 0.0f, 1.0f);
+      drawTriangle(mTriangle1Vertices);
+
+      Matrix.setIdentityM(mModelMatrix, 0);
+      Matrix.translateM(mModelMatrix, 0, 0.0f, -1.0f, 0.0f);
+      Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
+      Matrix.rotateM(mModelMatrix, 0, angleInDregrees, 0.0f, 0.0f, 1.0f);
+      drawTriangle(mTriangle2Vertices);
+
+      Matrix.setIdentityM(mModelMatrix, 0);
+      Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
+      Matrix.rotateM(mModelMatrix, 0, 90.0f, 0.0f, 1.0f, 0.0f);
+      Matrix.rotateM(mModelMatrix, 0, angleInDregrees, 0.0f, 0.0f, 1.0f);
+      drawTriangle(mTriangle3Vertices);
+    }
   }
 
   private String readAsset(String assetPath) {
